79ae68fa4ee5164ce06a64afc605720922d5677a
mandheer007 <mandheer2008@gmail.com>
2021-06-11 15:42:01 +0530

    Adding git files.

---
+++ Test Suites/Basic.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description>Basic Test suite for CURA</description>
+   <name>Basic</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>de2d02b4-afc3-43d5-8bd8-542e1021c80c</testSuiteGuid>
+   <testCaseLink>
+      <guid>4f633088-c1b7-408d-8a4c-164038aa202d</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Basic</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
---
+++ Test Suites/New Test Suite.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>New Test Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>0ce43c0a-ede1-482f-86df-bf6624496990</testSuiteGuid>
+   <testCaseLink>
+      <guid>4183a800-dd21-4b70-ab7a-1163ab72b02f</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test Case 2</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
